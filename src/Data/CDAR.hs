bbe104f2043d2d7b44143bb17b2a46acb695df37
Jens Blanck <jens.blanck@gmail.com>
2014-04-27 21:51:01 +0200

    central module that reexports all the other

---
+++ src/Data/CDAR.hs
@@ -0,0 +1,20 @@
+module Data.CDAR (module Data.CDAR.Approx
+                 ,module Data.CDAR.BR
+                 ,module Data.CDAR.CR
+                 ,module Data.CDAR.Dyadic
+                 ,module Data.CDAR.Extended
+                 ,module Data.CDAR.IntegerLog
+                 ,module Data.CDAR.Interval
+                 ,module Data.CDAR.POrd) where
+
+import qualified Data.CDAR.Approx as Approx
+import Data.CDAR.Approx hiding (toDouble)
+import qualified Data.CDAR.BR as BR
+import Data.CDAR.BR
+import qualified Data.CDAR.CR as CR
+import Data.CDAR.CR hiding (Resources, toDouble, bumpLimit, ok, resources, startLimit)
+import Data.CDAR.Dyadic
+import Data.CDAR.Extended
+import Data.CDAR.IntegerLog
+import Data.CDAR.Interval hiding (approximatedBy, better, centre, diameter, exact, lowerBound, upperBound)
+import Data.CDAR.POrd

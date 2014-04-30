42289ca870cc27b289c31f67d5c7eca6faad53ec
Jens Blanck <jens.blanck@gmail.com>
2014-04-30 20:57:24 +0200

    transferred Approx tests

--- test/Test.hs
+++ test/Test.hs
@@ -6,6 +6,7 @@ import Test.Tasty.HUnit
 import Test.SmallCheck.Series as SC
 import GHC.Generics
 
+import Control.Monad (liftM3)
 import Data.Functor
 
 import Data.List
@@ -36,9 +37,9 @@ instance Arbitrary Dyadic where
 		   return (a :^ s)
 
 propertiesD :: TestTree
-propertiesD = testGroup "Properties of Dyadic" [scPropsD, qcPropsD]
+propertiesD = testGroup "Properties of Dyadic" [scPropD, qcPropD]
 
-scPropsD = testGroup "(checked by SmallCheck)"
+scPropD = testGroup "(checked by SmallCheck)"
   [ SC.testProperty "compare" $
       \x y -> compare (x :: Dyadic) (y :: Dyadic) == (compare (toRational x) (toRational y))
   , SC.testProperty "multiplication" $
@@ -64,7 +65,7 @@ scPropsD = testGroup "(checked by SmallCheck)"
   , SC.testProperty "sqrt" $ \d@(a :^ s) -> sqrtD (min 0 (2*s)) (d*d) == abs d
   ]
 
-qcPropsD = testGroup "(checked by QuickCheck)"
+qcPropD = testGroup "(checked by QuickCheck)"
   [ QC.testProperty "compare" $ 
       \x y -> compare (x :: Dyadic) (y :: Dyadic) == (compare (toRational x) (toRational y))
   , QC.testProperty "multiplication" $
@@ -89,16 +90,88 @@ qcPropsD = testGroup "(checked by QuickCheck)"
   ]
 
 approx :: TestTree
-approx = testGroup "Approx" [scPropA]
+approx = testGroup "Approx" [scPropA, qcPropA]
 
 instance Monad m => Serial m Approx where
   series = cons0 Bottom \/ (tripleToApprox <$> series)
     where
       tripleToApprox (m, SC.NonNegative e, s) = Approx m e s
 
+instance Arbitrary Approx where
+    arbitrary = frequency [(10,
+                            liftM3 Approx
+                                   arbitrary 
+                                   (return 0)
+                                   (choose (-100,20))),
+                           (55,
+                            liftM3 Approx
+                                   arbitrary 
+                                   (elements [1,2,3,254,255,256,257,510,511,512,513,767,768,1020,1021,1022,1023,1024,1025])
+
+                                   (choose (-100,20))),
+                           (30,
+                            liftM3 Approx
+                                   arbitrary 
+                                   (choose (0,1025))
+                                   (choose (-100,20))),
+                           (5, return Bottom)]
+
 scPropA :: TestTree
 scPropA = testGroup "(checked by smallcheck)"
-  [ SC.testProperty "Equal" $ \a -> (a :: Approx) == a
+  [ SC.testProperty "read . show = id" $ \a -> (a :: Approx) == read (show a)
+  , SC.testProperty "fromEDI . toEDI = id" $ \a -> (a :: Approx) == fromEDI (toEDI a)
+  , SC.testProperty "diameter" $ \a -> diameter a == upperBound a - lowerBound a
+  , SC.testProperty "fromDyadic exact and centre" $ \d -> let a = fromDyadic d in exact a && d == centre a
+  , SC.testProperty "multiplication" $ \a b -> (toEDI (a*b) == (toEDI a) * (toEDI b))
+  , SC.testProperty "addition" $ \a b -> (toEDI (a+b) == (toEDI a) + (toEDI b))
+  , SC.testProperty "abs negate sign" $ \a -> approximatedBy 0 $ (a ::Approx) + (negate (signum a) * (abs a))
+  , SC.testProperty "a+(-a) contains 0" $ \a -> 0 `approximatedBy` ((a :: Approx) + negate a)
+  , SC.testProperty "toApprox r contains r" $ \r -> approximatedBy r $ toApprox 20 (r :: Rational)
+  , SC.testProperty "1/(1/a) contains a" $ \a -> not (0 `approximatedBy` a) SC.==> 
+                                                       a `better` (1/(1/a))
+  , SC.testProperty "a*(1/a) contains 1" $ \a -> not (0 `approximatedBy` a) SC.==> 1 `approximatedBy` (a*(1/a))
+  , SC.testProperty "boundErrorTerm" $
+    \a -> case a of {Approx _ _ _ -> let b@(Approx _ e _) = boundErrorTerm a
+		                    in (a `better` b) && (e < errorBound);
+                     Bottom       -> better a (boundErrorTerm a)}
+  , SC.testProperty "limitSize" $
+    \a -> case a of {Approx _ _ _ -> let b@(Approx _ _ s) = limitSize 2 a
+		                    in (a `better` b) && (s >= -2);
+                     Bottom       -> better a (limitSize 2 a)}
+  , SC.testProperty "sqrt" $ \a -> let b = abs a in better b $ (sqrtA 0 b)^2
+  ]
+
+qcPropA :: TestTree
+qcPropA = testGroup "(checked by smallcheck)"
+  [ QC.testProperty "read . show = id" $ \a -> (a :: Approx) == read (show a)
+  , QC.testProperty "fromEDI . toEDI = id" $ \a -> (a :: Approx) == fromEDI (toEDI a)
+  , QC.testProperty "diameter" $ \a -> diameter a == upperBound a - lowerBound a
+  , QC.testProperty "fromDyadic exact and centre" $ \d -> let a = fromDyadic d in exact a && d == centre a
+  , QC.testProperty "multiplication" $ \a b -> (toEDI (a*b) == (toEDI a) * (toEDI b))
+  , QC.testProperty "addition" $ \a b -> (toEDI (a+b) == (toEDI a) + (toEDI b))
+  , QC.testProperty "abs negate sign" $ \a -> approximatedBy 0 $ (a ::Approx) + (negate (signum a) * (abs a))
+  , QC.testProperty "addition precision" $ \a b -> 
+      collect (precision (boundErrorTerm (a+b)) - (precision a `min` precision b))
+	      True
+{-
+  , QC.testProperty "multiplication precision" $ \a b -> 
+      collect (precision (boundErrorTerm (a*b)) - (precision a `min` precision b))
+	      True
+-}
+  , QC.testProperty "a+(-a) contains 0" $ \a -> 0 `approximatedBy` ((a :: Approx) + negate a)
+  , QC.testProperty "toApprox r contains r" $ \r -> approximatedBy r $ toApprox 20 (r :: Rational)
+  , QC.testProperty "1/(1/a) contains a" $ \a -> not (0 `approximatedBy` a) QC.==> 
+                                                       a `better` (1/(1/a))
+  , QC.testProperty "a*(1/a) contains 1" $ \a -> not (0 `approximatedBy` a) QC.==> 1 `approximatedBy` (a*(1/a))
+  , QC.testProperty "boundErrorTerm" $
+    \a -> case a of {Approx _ _ _ -> let b@(Approx _ e _) = boundErrorTerm a
+		                    in (a `better` b) && (e < errorBound);
+                     Bottom       -> better a (boundErrorTerm a)}
+  , QC.testProperty "limitSize" $
+    \a -> case a of {Approx _ _ _ -> let b@(Approx _ _ s) = limitSize 2 a
+		                    in (a `better` b) && (s >= -2);
+                     Bottom       -> better a (limitSize 2 a)}
+  , QC.testProperty "sqrt" $ \a -> let b = abs a in better b $ (sqrtA 0 b)^2
   ]
 
 unitTests = testGroup "Unit tests"

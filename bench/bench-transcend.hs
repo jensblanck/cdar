262c8e278a995c9182e400e45126a63fe4543e66
Jens Blanck <jens.blanck@gmail.com>
2014-06-15 17:02:54 +0100

    added exp benchmark

---
+++ bench/bench-transcend.hs
@@ -0,0 +1,26 @@
+import Criterion.Main
+import Criterion.Config
+
+import Data.CDAR
+
+suite :: [Benchmark]
+suite =
+    [ bgroup "exp 1"
+      [ bench "200" $ whnf (require 200) (exp 1)
+      , bench "2000" $ whnf (require 2000) (exp 1)
+      , bench "20000" $ whnf (require 20000) (exp 1)
+      ]
+    , bgroup "exp ~1"
+      [ bench "200" $ whnf (require 200) (exp (1 + BR (repeat (Approx 0 1 (-250)))))
+      , bench "2000" $ whnf (require 2000) (exp (1 + BR (repeat (Approx 0 1 (-2050)))))
+--      , bench "20000" $ whnf (require 20000) (exp $ 1 + (BR . repeat $ Approx 0 1 (-25000)))
+      ]
+    , bgroup "exp n"
+      [ bench "200" $ whnf (require 200) (exp 200)
+      , bench "2000" $ whnf (require 200) (exp 2000)
+      , bench "20000" $ whnf (require 200) (exp 20000)
+      ]
+    ]
+
+main :: IO ()
+main = defaultMain suite
---
+++ examples/BinarySplit.hs
@@ -0,0 +1,16 @@
+module BinarySplit where
+
+import Data.Ratio
+
+import Data.CDAR
+
+pq a b
+    | b - a > 3 = let m = (a+b+1) `div` 2
+                      (pam, qam) = pq a m
+                      (pmb, qmb) = pq m b
+                  in (pam*qmb+pmb, qam*qmb)
+    | otherwise = (sum $ scanl1 (*) [b,b-1..a+1], product [a+1..b])
+
+euler n = let (p,q) = pq 0 n
+          in map showA . take 3 . (\(BR l) -> l) $ (fromIntegral p :: BR Approx) / fromIntegral q
+
